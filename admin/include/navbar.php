0074572d9084ad4711211f4f1392ecae5f48b2d8
Monoget <monoget2@gmail.com>
2022-11-28 22:35:47 +0600

    update admin

--- admin/include/navbar.php
+++ admin/include/navbar.php
@@ -6,10 +6,11 @@
         <ul class="metismenu" id="menu">
             <li class="dropdown header-profile">
                 <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
-                    <img src="public/images/profile/pic1.jpg" width="20" alt=""/>
+                    <?php $data = $db_handle->runQuery("SELECT * FROM admin_login where id={$_SESSION['userid']}"); ?>
+                    <img src="../<?php echo $data[0]["image"]; ?>" width="20" alt=""/>
                     <div class="header-info ms-3">
-                        <span class="font-w600 ">Hi,<b>William</b></span>
-                        <small class="text-end font-w400">william@gmail.com</small>
+                        <span class="font-w600 "><b><?php echo $data[0]["name"]; ?></b></span>
+                        <small class="text-end font-w400"><?php echo $data[0]["email"]; ?></small>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end">
